5fbbc2f0bccbea6d58d00113bf328c0a235dd584
Gitthuma <george.githuma@gmail.com>
2021-04-29 00:13:03 +0300

    Referenced paragraph element and stored it in a variable

--- code/main.js
+++ code/main.js
@@ -6,3 +6,7 @@ const form = document.querySelector('form');
 
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
+
+/*Reference paragraph element and store it in a variable*/
+
+const para = document.querySelector('p');
