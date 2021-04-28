f56179c776a5d362c1b22cceb3e3b64cd91bd7b9
Gitthuma <george.githuma@gmail.com>
2021-04-29 00:10:41 +0300

    Referenced form inputs and stored them in a variable

--- code/main.js
+++ code/main.js
@@ -1,3 +1,8 @@
 /*Reference form element and store it in a variable*/
 
 const form = document.querySelector('form');
+
+/*Reference form inputs and store them in a variable*/
+
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
