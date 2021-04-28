c5e0c04353078190c93c4d29c50771c683e86413
Gitthuma <george.githuma@gmail.com>
2021-04-29 00:21:21 +0300

    Implemented a simple check inside an onsubmit event handler (the submit event is fired on a form when it is submitted) that tests whether the text fields are empty. If they are, we call the preventDefault() function on the event object — which stops the form submission — and then display an error message in the paragraph below our form to tell the user what's wrong

--- code/main.js
+++ code/main.js
@@ -10,3 +10,12 @@ const lname = document.getElementById('lname');
 /*Reference paragraph element and store it in a variable*/
 
 const para = document.querySelector('p');
+
+/*Implement a simple check inside an onsubmit event handler (the submit event is fired on a form when it is submitted) that tests whether the text fields are empty. If they are, we call the preventDefault() function on the event object — which stops the form submission — and then display an error message in the paragraph below our form to tell the user what's wrong*/
+
+form.onsubmit = function(e){
+    if(fname.value === '' || lname.value === '') {
+        e.preventDefault();
+        para.textContent = 'You need to fill in both names!';
+    }
+}
